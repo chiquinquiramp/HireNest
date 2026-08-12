20e52aa2dcc08d3078398ab769b160e66cb9e1c9
Chiquinquira Maldonado <sarielkysmp@gmail.com>
2026-08-11 22:14:44 -0400

    Fix spelling of 'EveryDay' in Hero component
    Corrected the spelling of 'EveryDay' to 'Everyday' in the Hero component.

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -5,7 +5,7 @@ function Hero () {
     return (
         <>
         <h1>HireNest</h1>
-        <p>Trusted Help for EveryDay Life</p>
+        <p>Trusted Help for Everyday Life</p>
         
         <button type="button">Find Trusted Help</button>
         <button type="button">Find Opportunities</button>
