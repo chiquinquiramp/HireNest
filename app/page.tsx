760c297e6c72f9b8b0e43255466d369820e24798
Chiquinquira Maldonado <sarielkysmp@gmail.com>
2026-08-12 21:57:45 -0400

    Fix import path for WhyEdenest component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/navbar/navbar";
 import Hero from "@/components/hero/Hero";
-import WhyEdenest from "@/components/why-edenest/WhyEdenest";
+import WhyEdenest from "@/components/why-edenest/Why-Edenest";
 import HowItWorks from "@/components/how-it-works/HowItWorks";
 import Services from "@/components/services/Services";
 import FAQ from "@/components/faq/FAQ";
